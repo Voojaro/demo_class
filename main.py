e90850735f78e80b8fa90940d39a717ac404bcf5
Voojaro <khuntharo.oeung@gmail.com>
2025-06-15 09:32:23 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pygame import *
+
+
+background = (200,255,255)
+
+width = 500
+height = 500
+
+window = display.set_mode()
+window.fill(background)
+
+clock = time.Clock()
+
+run = True
+
+while run:
+    
